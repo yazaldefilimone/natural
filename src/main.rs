1717b9621543f461482939a221d33a85213679ed
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-02 22:15:03 +0200

    wire custom test framework in main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 #![no_std]
 #![no_main]
-
+#![feature(custom_test_frameworks)]
+#![test_runner(natural::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 
 mod console;
 mod drivers;
+mod serial;
 
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+  println!("Hello World{}", "!");
+
+  #[cfg(test)]
+  test_main();
+
+  loop {}
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-  println!("Hello World{}", "!");
-  loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  natural::test_panic_handler(info)
 }
