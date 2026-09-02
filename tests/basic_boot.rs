e8314c97ef78eb913ec0f001c8d8e6da8f3d4ac4
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-02 22:15:03 +0200

    add basic_boot and should_panic tests

---
+++ tests/basic_boot.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(natural::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use natural::println;
+
+#[test_case]
+fn test_println() {
+  println!("test_println output");
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  natural::test_panic_handler(info)
+}
