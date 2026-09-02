c76ffb88243d1ed7d3186e78b95c0b47e5f79064
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-02 15:24:56 +0200

    print hello world via vga

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![no_std]
 #![no_main]
+
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+mod console;
+mod drivers;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
   loop {}
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+  println!("Hello World{}", "!");
   loop {}
 }
