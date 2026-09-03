1f9c9ff306f8c26a6f953d26808fec252bfd5888
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-04 00:00:10 +0200

    refactor: format macros

--- src/console/macros.rs
+++ src/console/macros.rs
@@ -1,26 +1,21 @@
 #[macro_export]
 macro_rules! serial_print {
-    ($($arg:tt)*) => {
-        $crate::drivers::serial::print(
-            format_args!($($arg)*)
-        );
-    };
+  ($($arg:tt)*) => {
+    $crate::drivers::serial::print(format_args!($($arg)*))
+  };
 }
 
 #[macro_export]
 macro_rules! serial_println {
-    () => {
-        $crate::serial_print!("\n")
-    };
+  () => {
+    $crate::serial_print!("\n")
+  };
 
-    ($fmt:expr) => {
-        $crate::serial_print!(concat!($fmt, "\n"))
-    };
+  ($fmt:expr) => {
+    $crate::serial_print!(concat!($fmt, "\n"))
+  };
 
-    ($fmt:expr, $($arg:tt)*) => {
-      $crate::serial_print!(
-        concat!($fmt, "\n"),
-        $($arg)*
-      )
-    };
+  ($fmt:expr, $($arg:tt)*) => {
+    $crate::serial_print!(concat!($fmt, "\n"), $($arg)*)
+  };
 }
