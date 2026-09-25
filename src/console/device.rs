ac748ff366d1596ff5dfe3adaee3be4f71ba9945
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-25 09:05:10 +0200

    refactor: simplify console trait

--- src/console/device.rs
+++
@@ -1,9 +0,0 @@
-pub trait Console {
-  fn write_byte(&mut self, byte: u8);
-
-  fn write(&mut self, text: &str) {
-    for byte in text.bytes() {
-      self.write_byte(byte);
-    }
-  }
-}
--- src/console/mod.rs
+++ src/console/mod.rs
@@ -1,3 +1,11 @@
-mod device;
 mod macros;
-pub use device::*;
+
+pub trait Console {
+  fn write_byte(&mut self, byte: u8);
+
+  fn write(&mut self, text: &str) {
+    for byte in text.bytes() {
+      self.write_byte(byte);
+    }
+  }
+}
