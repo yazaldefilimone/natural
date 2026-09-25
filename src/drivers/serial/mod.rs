33e42e1c6d0136c9cfef0f16e775a3616bd5d465
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-25 11:39:32 +0200

    refactor: prevent deadlocks in print macros

--- src/drivers/serial/mod.rs
+++ src/drivers/serial/mod.rs
@@ -5,8 +5,12 @@ pub use com::COM1;
 #[doc(hidden)]
 pub fn print(args: core::fmt::Arguments) {
   use core::fmt::Write;
-  COM1
-    .lock()
-    .write_fmt(args)
-    .expect("printing to serial failed");
+  use x86_64::instructions::interrupts;
+
+  interrupts::without_interrupts(|| {
+    COM1
+      .lock()
+      .write_fmt(args)
+      .expect("printing to serial failed");
+  });
 }
