737928815336fc4c4f863d2b5b43a038b5f578b6
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-09-25 11:39:54 +0200

    test: add println_output and protect vga tests

--- tests/kernel/drivers/video/vga/color.rs
+++ tests/kernel/drivers/video/vga/color.rs
@@ -1,18 +1,23 @@
 use natural::drivers::video::vga::WRITER;
 use natural::drivers::video::vga::color::{Color, ColorCode};
+use x86_64::instructions::interrupts;
 
 #[test_case]
 fn keeps_color() {
-  let writer = WRITER.lock();
-  assert_eq!(writer.color(), ColorCode::new(Color::Yellow, Color::Black));
+  interrupts::without_interrupts(|| {
+    let writer = WRITER.lock();
+    assert_eq!(writer.color(), ColorCode::new(Color::Yellow, Color::Black));
+  });
 }
 
 #[test_case]
 fn changes_color() {
-  let mut writer = WRITER.lock();
-  let new_color = ColorCode::new(Color::LightGreen, Color::Black);
-  writer.set_color(new_color);
-  assert_eq!(writer.color(), new_color);
-  // Restore default color
-  writer.set_color(ColorCode::new(Color::Yellow, Color::Black));
+  interrupts::without_interrupts(|| {
+    let mut writer = WRITER.lock();
+    let new_color = ColorCode::new(Color::LightGreen, Color::Black);
+    writer.set_color(new_color);
+    assert_eq!(writer.color(), new_color);
+    // Restore default color
+    writer.set_color(ColorCode::new(Color::Yellow, Color::Black));
+  });
 }
--- tests/kernel/drivers/video/vga/writer.rs
+++ tests/kernel/drivers/video/vga/writer.rs
@@ -1,39 +1,56 @@
+use core::fmt::Write;
+use natural::drivers::video::vga::color::{Color, ColorCode};
 use natural::drivers::video::vga::{BUFFER_HEIGHT, WRITER};
-use natural::println;
+use x86_64::instructions::interrupts;
+
+#[test_case]
+fn test_println_output() {
+  let s = "A single line test string that fits on one line";
+  interrupts::without_interrupts(|| {
+    let mut writer = WRITER.lock();
+    writeln!(writer, "\n{}", s).expect("writeln failed");
+    for (i, c) in s.chars().enumerate() {
+      let cell = writer.cell(BUFFER_HEIGHT - 2, i);
+      assert_eq!(char::from(cell.ascii), c);
+    }
+  });
+}
+
+#[test_case]
+fn test_println_many() {
+  for _ in 0..200 {
+    natural::println!("test_println_many output");
+  }
+}
 
 #[test_case]
 fn writes_char() {
-  let mut writer = WRITER.lock();
-  writer.newline();
-  writer.write_byte(b'X');
-  let cell = writer.cell(BUFFER_HEIGHT - 1, 0);
-  assert_eq!(cell.ascii, b'X');
-  writer.newline();
+  interrupts::without_interrupts(|| {
+    let mut writer = WRITER.lock();
+    writer.newline();
+    writer.write_byte(b'X');
+    let cell = writer.cell(BUFFER_HEIGHT - 1, 0);
+    assert_eq!(cell.ascii, b'X');
+    writer.newline();
+  });
 }
 
 #[test_case]
-fn writes_string() {
-  let s = "Hello Natural OS";
-  println!("{}", s);
-  for (i, c) in s.chars().enumerate() {
-    let cell = WRITER.lock().cell(BUFFER_HEIGHT - 2, i);
-    assert_eq!(char::from(cell.ascii), c);
-  }
-}
+fn writes_with_custom_color() {
+  interrupts::without_interrupts(|| {
+    let mut writer = WRITER.lock();
+    let original_color = writer.color();
+    let test_color = ColorCode::new(Color::Cyan, Color::DarkGray);
 
-#[test_case]
-fn prints_text() {
-  println!("prints_text output");
-}
+    writer.set_color(test_color);
+    writer.newline();
+    writer.write_byte(b'Z');
 
-#[test_case]
-fn prints_format() {
-  println!("Formatted: {} + {} = {}", 2, 2, 4);
-}
+    let cell = writer.cell(BUFFER_HEIGHT - 1, 0);
+    assert_eq!(cell.ascii, b'Z');
+    assert_eq!(cell.color, test_color);
 
-#[test_case]
-fn prints_many_lines() {
-  for _ in 0..200 {
-    println!("prints_many_lines output");
-  }
+    writer.set_color(original_color);
+    writer.newline();
+  });
 }
